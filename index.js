40a4f4edb6d9f6f8242cfea40219909f951c685f
steffafregele-oss <steffafregele@gmail.com>
2025-11-12 20:50:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ client.on("messageCreate", async (message) => {
 
       const embed = new EmbedBuilder()
         .setColor(0x000000)
-        .setThumbnail("https://cdn.discordapp.com/emojis/1438178217247768699.gif") // thumbnail animat
+        .setThumbnail(targetUser.displayAvatarURL({ dynamic: true })) // <<< thumbnail schimbat
         .setDescription(
           `-- <a:63804crownblack:1438178018144161842> NEXUS BOT <a:63804crownblack:1438178018144161842> --\n\n` +
           `<a:5228_Seta:1438177992168968343> **User:** ${userName}\n\n` +
@@ -147,7 +147,7 @@ client.on("messageCreate", async (message) => {
 
       const embed = new EmbedBuilder()
         .setColor(0x000000)
-        .setThumbnail("https://cdn.discordapp.com/emojis/1438178217247768699.gif") // thumbnail animat
+        .setThumbnail(targetUser.displayAvatarURL({ dynamic: true })) // <<< thumbnail schimbat
         .setDescription(
           `-- <a:63804crownblack:1438178018144161842> NEXUS BOT <a:63804crownblack:1438178018144161842> --\n\n` +
           `<a:5228_Seta:1438177992168968343> **User:** ${userName}\n\n` +
@@ -199,7 +199,7 @@ client.on("messageCreate", async (message) => {
 
       const embed = new EmbedBuilder()
         .setColor(0x000000)
-        .setThumbnail("https://cdn.discordapp.com/emojis/1438177965962690732.gif") // thumbnail animat
+        .setThumbnail("https://cdn.discordapp.com/emojis/1438178217247768699.gif") // thumbnail animat
         .setDescription(
           `-- <a:63804crownblack:1438178018144161842> NEXUS BOT <a:63804crownblack:1438178018144161842> --\n\n` +
           `<a:5228_Seta:1438177992168968343> **${MAIN_SITE_NAME}**\n` +
